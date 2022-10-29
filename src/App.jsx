db6f0a1e0466e5e2776caf6bb6ad08b72b7fc17f
Ishrat <47534248+IshratFatima18@users.noreply.github.com>
2022-10-29 22:53:53 +0500

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import styles from './style'
+
+const App = () => (
+    <div className="bg-primary w-full overflow-hidden">
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          Navbar
+        </div>
+      </div>
+
+    <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        Hero
+      </div>
+    </div>
+
+    <div className={`bg-primary ${styles.flexStart} ${styles.paddingX}`}>
+      <div className={`${styles.boxWidth}`}>
+        Stats
+        Business
+        Billing
+        CardDeal
+        Testimonial
+        Clients
+        CTA
+        Footer
+      </div>
+    </div>
+
+    </div>
+);
+export default App
