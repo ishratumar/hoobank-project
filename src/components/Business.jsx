db1ce2c5bb0d510511d81a950cf939fe4862ffb5
Ishrat <47534248+IshratFatima18@users.noreply.github.com>
2022-11-07 07:50:14 +0500

    add business section cards

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -2,6 +2,23 @@ import { features } from "../constants";
 import styles, { layout} from "../style";
 import Button from './Button';
 
+const FeatureCard = ({ icon, title, content, index }) => (
+  <div className={` flex flex-row p-6 rounded-[120px] ${index !== features.length - 1 ? "mb-6" : "mb-0"} feature-card`}>
+    <div className={`w-[64px] h-[64px] rounded-full 
+    ${styles.flexCenter} bg-dimBlue`}>
+      <img src={icon} alt="icon" className="w-[50%/] h-[50%] object-contain" />
+    </div>
+    <div className="flex- flex flex-col ml-3">
+      <h4 className="font-poppins font-semibold text-white text-[180px] leading-[24px] mb-1">
+        {title}
+      </h4>
+      <p className="font-poppins font-normal text-dimwhite text-[16px] leading-[24px] mb-1">
+        {content}
+      </p>
+    </div>
+  </div>
+)
+
 const Business = () => {
   return (
     <section id="features" className={layout.section}>
@@ -11,6 +28,11 @@ const Business = () => {
         <Button styles="mt-10" />
       </div>
       
+      <div className={`${layout.sectionImg} flex-col`}>
+        {features.map((feature, index) =>(
+          <FeatureCard key={feature.id} {...feature} index={index} />
+        ))}
+      </div>
     </section>
   )
 }
