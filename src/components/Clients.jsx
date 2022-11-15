54fb4b4db64e25cf5f369d42ff851ff94a5ff92c
Ishrat <47534248+IshratFatima18@users.noreply.github.com>
2022-11-15 14:33:54 +0500

    add footer

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -6,7 +6,7 @@ const Clients = () => (
       <div className={`${styles.flexCenter} flex-wrap w-full`}>
         {clients.map((client) =>(
           <div key={client.id} className={`flex-1 ${styles.flexCenter} sm:min-w-[192px] min-w-[120px]`}>
-            <img src={client.logo} alt="client" className="sm-w-[192px] w-[100px] object-contain" />
+            <img src={client.logo} alt="client" className="sm-w-[192px] w-[100px] object-contain cursor-pointer" />
 
           </div>
         ))}
