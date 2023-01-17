f3e8c2f4a9db9a64d967e15798182e6a85ffceb0
Ishrat <47534248+ishratUmar18@users.noreply.github.com>
2023-01-17 13:56:39 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,7 @@ const App = () => (
     <div className={`bg-primary ${styles.flexCenter} ${styles.paddingX}`}>
       <div className={`${styles.boxWidth}`}>
         <Stats />
+        <Business /> 
         {/* <Business /> */}
         <Billing />
         <CardDeal />
